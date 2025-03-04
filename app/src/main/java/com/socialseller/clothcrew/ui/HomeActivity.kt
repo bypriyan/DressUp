cd3669ea36853858bb3d1fd4ec432b0a9eb3b051
priyanshu <104priyanshu@gmail.com>
2025-03-04 17:40:29 +0530

    nav drawer done

--- app/src/main/java/com/socialseller/clothcrew/ui/HomeActivity.kt
+++ app/src/main/java/com/socialseller/clothcrew/ui/HomeActivity.kt
@@ -3,8 +3,11 @@ package com.socialseller.clothcrew.ui
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
@@ -15,25 +18,42 @@ import com.socialseller.clothcrew.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityHomeBinding
-    lateinit var bottomNavigationView: BottomNavigationView
-    lateinit var navController: NavController
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navController: NavController
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityHomeBinding.inflate(layoutInflater)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //init
-        bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        navController = Navigation.findNavController(this, R.id.frameLayout);
-        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+        // Initialize DrawerLayout
+        drawerLayout = findViewById(R.id.drawerLayout)
 
-        binding.locationStore.setOnClickListener{
+        // Initialize Navigation Controller
+        navController = Navigation.findNavController(this, R.id.frameLayout)
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
+
+        // Open Drawer when navDrawer icon is clicked
+        binding.navDrawer.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        binding.locationStore.setOnClickListener {
             startActivity(Intent(this, StoreLocationActivity::class.java))
         }
 
+        // Handle back button press with OnBackPressedDispatcher
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                } else {
+                    isEnabled = false
+                    onBackPressedDispatcher.onBackPressed()
+                }
+            }
+        })
     }
-
 }
